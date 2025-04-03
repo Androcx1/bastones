221c295dc8ec75228aa2b32432f149a6e512cf7b
Androcx1 <ivancho03.rico@gmail.com>
2025-04-02 23:48:40 -0600

    Si no queda me rindo

--- bastonesceoxv2/src/firebase.js
+++ bastonesceoxv2/src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getDatabase } from "firebase/database"; // 👈 Realtime Database
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
+  apiKey: "AIzaSyBASfSd6hALKmW4bGYetoAK0aJnkg-obrs",
   authDomain: "bastones-ceox-5463c.firebaseapp.com",
   databaseURL: "https://bastones-ceox-5463c-default-rtdb.firebaseio.com", // ✅ ESTE LINK ES CLAVE
   projectId: "bastones-ceox-5463c",
   storageBucket: "bastones-ceox-5463c.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  messagingSenderId: "497016388925",
+  appId: "1:497016388925:web:65d7d2c06400ad699c9954"
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
